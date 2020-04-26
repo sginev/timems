dba03f9a30b99617470d52d8fff51f7d34392b9e
Stefan Ginev <choephix@gmail.com>
2020-04-26 04:40:27 +0000

    show locked account message on about section

--- frontend/src/pages/inshell/About.tsx
+++ frontend/src/pages/inshell/About.tsx
@@ -16,14 +16,28 @@ export default function AboutPage() {
         <h2>Time Management Test Project</h2>
         <br/>
         <br/>
-        <h4>Welcome, <b>{ myUser.username }</b>!</h4>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <p className="App-link" onClick={ () => history.push("/my-entries") } >
-          Click here to view your entries.
-        </p>
+        { myUser.role > 0 ? (
+          <>
+            <h4>Welcome, <b>{ myUser.username }</b>!</h4>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <p className="App-link" onClick={ () => history.push("/my-entries") } >
+              Click here to view your entries.
+            </p>
+          </>
+        ) : (
+          <>
+            <h3>Your account has been <b>locked</b>!</h3>
+            <br/>
+            <br/>
+            <br/>
+            <p>
+              Please contact an administrator or a user manager if you think this is a mistake.
+            </p>
+          </>
+        ) }
       </PageContentBodyComponent>
     </div>
   )
